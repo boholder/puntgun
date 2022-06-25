07f2e059fe4f22c5acfa1e148abb89788496a879
boholder <bottleholder@anche.no>
2022-06-25 21:38:15 +0800

    Add follower and following user filter rule

---
+++ puntgun/rules/user/filter_rules.py
@@ -0,0 +1,51 @@
+import sys
+
+from pydantic import BaseModel, root_validator
+
+from rules.user import User
+
+
+class UserFilterRule(object):
+    """
+    Holds a set of value from plan configuration for constructing a predicative rule.
+
+    Takes **one** user instance each time and judges if this user's data triggers(fits) its rule's condition.
+    """
+
+
+class NumericUserFilterRule(BaseModel):
+    """
+    A rule that checks if a user's numeric data fits a certain condition.
+    """
+    less_than = float(sys.maxsize)
+    more_than = float(-sys.maxsize - 1)
+
+    @root_validator(pre=True)
+    def there_should_be_fields(cls, values):
+        if not values.get('less_than') and not values.get('more_than'):
+            raise ValueError('At least one of "less_than" or "more_than" should be specified.')
+        return values
+
+    @root_validator
+    def compare(cls, values):
+        lt, mt = values.get('less_than'), values.get('more_than')
+        if lt <= mt:
+            raise ValueError(f"'less_than'({lt}) should be bigger than 'more_than'({mt})")
+        return values
+
+    def __call__(self, num):
+        return self.more_than < num < self.less_than
+
+
+class FollowerUserFilterRule(NumericUserFilterRule, UserFilterRule):
+    """Check user's follower count."""
+
+    def __call__(self, user: User):
+        return super().__call__(user.followers_count)
+
+
+class FollowingUserFilterRule(NumericUserFilterRule, UserFilterRule):
+    """Check user's following count."""
+
+    def __call__(self, user: User):
+        return super().__call__(user.following_count)
---
+++ tests/rules/test_user_filter_rules.py
@@ -0,0 +1,35 @@
+import pytest
+from hamcrest import assert_that, is_, contains_string
+from pydantic import ValidationError
+
+from rules.user.filter_rules import NumericUserFilterRule
+
+
+class TestNumericUserFilterRule:
+    @pytest.fixture()
+    def rule(self):
+        return lambda obj: NumericUserFilterRule.parse_obj(obj)
+
+    def test_check_number_order(self, rule):
+        with pytest.raises(ValidationError) as actual_error:
+            rule({'more_than': 10, 'less_than': 5})
+        assert_that(str(actual_error.value), contains_string('than'))
+
+    def test_single_less_than(self, rule):
+        r = rule({'less_than': 10})
+        assert_that(r(5), is_(True))
+        assert_that(r(20), is_(False))
+
+    def test_single_more_than(self, rule):
+        r = rule({'more_than': 10})
+        assert_that(r(20), is_(True))
+        assert_that(r(5), is_(False))
+
+    def test_both_less_more(self, rule):
+        r = rule({'less_than': 20, 'more_than': 10})
+        assert_that(r(15), is_(True))
+        assert_that(r(5), is_(False))
+        assert_that(r(25), is_(False))
+        # edge case (equal) result in False.
+        assert_that(r(10), is_(False))
+        assert_that(r(20), is_(False))
