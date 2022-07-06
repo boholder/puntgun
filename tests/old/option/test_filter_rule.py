aee48b7eb0c8947b4b73bc2c64a4c0816ef2a318
boholder <bottleholder@anche.no>
2022-07-06 15:14:18 +0800

    Finish rule sets. (without thread pool)

--- tests/old/option/test_filter_rule.py
+++ tests/old/option/test_filter_rule.py
@@ -9,12 +9,12 @@ class TestSearchFilterRule(TestCase):
     def test_build_one(self):
         rule = SearchFilterRule.build({'name': 'option!', 'query': 'q_text'})
         assert_that(rule.name, equal_to('option!'))
-        assert_that(rule.count, equal_to(100))
+        assert_that(rule.work_count, equal_to(100))
         assert_that(rule.query, equal_to('q_text'))
 
 
 class TestSearchQueryFilterRule(TestCase):
     def test_build_one(self):
         rule = SearchQueryFilterRule.build('q_text')
-        assert_that(rule.count, equal_to(100))
+        assert_that(rule.work_count, equal_to(100))
         assert_that(rule.query, equal_to('q_text'))
