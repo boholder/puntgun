731233375865ed51298c611e579dae7c8f23fa20
boholder <bottleholder@anche.no>
2022-07-18 20:34:21 +0800

    - Halfway write Record

--- puntgun/recorder.py
+++ puntgun/recorder.py
@@ -1,25 +1,41 @@
-from typing import Generic, TypeVar
+from datetime import datetime
 
 from pydantic import BaseModel
+
+
+class Record(BaseModel):
+    """
+    Record wrapper for a recordable object for format uniformity.
+    """
+    type: str
+    data: dict
+
+    def to_yaml(self) -> str:
+        """
+        Translate this record into a yaml-list-item format string.
+        "time" field for labeling record happening time.
+        """
+        # TODO untested
+
+        return f"""
+  - type: {self.type}
+    time: {datetime.now()}
+    data: 
+{[f'      - {k}: {v}' for k, v in self.data]}
+"""
 
 
 class Recordable(object):
     """Interface for objects that can be recorded into report file."""
 
-    def record(self) -> str:
+    def record(self) -> Record:
         """Record the object."""
         raise NotImplementedError
 
-
-_T = TypeVar('_T', bound=Recordable)
-
-
-class Record(Generic[_T], BaseModel):
-    """
-    Record wrapper for a recordable object.
-    Handling basic output formatting for format uniformity.
-    """
-    pass
+    @staticmethod
+    def parse_from_record(record: Record):
+        """Generate an instance from a record."""
+        raise NotImplementedError
 
 
 class Recorder(object):
