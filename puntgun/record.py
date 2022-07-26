5ebf9722d2b4643c4723fa53ffc78e0015b286e0
boholder <bottleholder@anche.no>
2022-07-26 16:46:59 +0800

    Finish Recorder

--- puntgun/record.py
+++ puntgun/record.py
@@ -16,6 +16,7 @@ Ok, speed is important. I changed the output format to json -
 I've heard about the effort different json parsing libraries have made.
 """
 from datetime import datetime
+from typing import List
 
 import orjson
 from loguru import logger
@@ -36,11 +37,8 @@ class Record(object):
         self.data = data
 
     def to_json(self):
-        """
-        Translate this record into a yaml-list-item format string.
-        "time" field for labeling record happening time.
-        """
-        return orjson.dumps({'type': self.name, 'time': datetime.now(), 'data': self.data})
+        """Translate this record into a yaml-list-item format string"""
+        return orjson.dumps({'type': self.name, 'data': self.data})
 
     @staticmethod
     def from_parsed_dict(conf: dict):
@@ -63,29 +61,24 @@ class Recordable(object):
         raise NotImplementedError
 
 
+COMMA = ','.encode('utf-8')
+PLACEHOLDER_ITEM = '{},'.encode('utf-8')
+
+# 1. an empty list item (an empty map) for pairing one comma character behind the last plan item
+# 2. one square bracket for closing the "plans" list
+# 3. one curly bracket for closing the root level dictionary
+REPORT_TAIL = '{}]}'.encode('utf-8')
+
+
 class Recorder(object):
     """
     Saving / loading execution results with a report file in json format.
     Annoying temporal coupling included, you must call methods in this class
     at proper time and following certain order to compose a correct json format output.
-    # TODO logger保证输出的次序，尤其是开始和结束的次序,用流去完成
     """
-    _comma = ','.encode('utf-8')
-    _placeholder_item = '{},'.encode('utf-8')
-
-    # 1. an empty list item (an empty map) for pairing one comma character behind the last record item
-    # 2. one square bracket for closing the "records" list
-    # 3. one curly bracket for closing the plan list item
-    # 4. one comma for waiting for next plan list item
-    _plan_record_tail = '{}]},'.encode('utf-8')
-
-    # 1. an empty list item (an empty map) for pairing one comma character behind the last plan item
-    # 2. one square bracket for closing the "plans" list
-    # 3. one curly bracket for closing the root level dictionary
-    _report_tail = '{}]}'.encode('utf-8')
 
     @staticmethod
-    def write_to_file(msg: bytes):
+    def _write_to_file(msg: bytes):
         logger.info(msg)
 
     @staticmethod
@@ -94,28 +87,24 @@ class Recorder(object):
         Log recordable instances into report file as normal log.
         """
         # write as a "records" list item
-        Recorder.write_to_file(recordable.to_record().to_json() + Recorder._comma)
+        Recorder._write_to_file(recordable.to_record().to_json() + COMMA)
 
     @staticmethod
-    def write_report_header():
+    def write_report_header(plans: List[Plan]):
         """
         This paragraph works as the report file content's header
         - for correctly formatting latter records in json format.
         """
+
         head = {'referring_document': '',  # TODO
                 'tool_version': config.puntgun_version,
                 'generate_time': datetime.now(),
-                # contains detail records:
-                #
-                # 'plans': [
-                # {'name': '...',
-                # 'records': [...]
-                # },
-                # ...
-                # ]
-                'plans': []}
-        Recorder.write_to_file(orjson.dumps(head, option=orjson.OPT_INDENT_2)[:-3]
-                               + Recorder._placeholder_item)
+                'plan_configuration': config.settings.get('plans', []),
+                'tool_configuration': config.settings.get('tool_config', {}),
+                'plan_ids': [{'name': p.name, 'id': p.id} for p in plans],
+                'records': []}
+
+        Recorder._write_to_file(orjson.dumps(head, option=orjson.OPT_INDENT_2)[:-3] + PLACEHOLDER_ITEM)
 
     @staticmethod
     def write_report_tail():
@@ -123,19 +112,7 @@ class Recorder(object):
         After all records are written, at the end of the program running,
         append this part to remain a corrct json format.
         """
-        Recorder.write_to_file(Recorder._report_tail)
-
-    @staticmethod
-    def write_plan_record_header(plan: Plan):
-        """Call it before running every plan."""
-        head = {'name': plan.name, 'records': []}
-        Recorder.write_to_file(orjson.dumps(head, option=orjson.OPT_INDENT_2)[:-3]
-                               + Recorder._placeholder_item)
-
-    @staticmethod
-    def write__plan_record_tail():
-        """Call it after a plan running finish."""
-        Recorder.write_to_file(Recorder._plan_record_tail)
+        Recorder._write_to_file(REPORT_TAIL)
 
     @staticmethod
     def load_report(file_content: bytes) -> dict:
@@ -151,10 +128,13 @@ class Recorder(object):
         # But if the program stopped by user, unexpected exceptions...
         # the file's content is json-format-broken,
         # we'll fix it manually as we know what it's missing.
+        #
         # lambda for lazy calculating
         cases = [lambda: file_content,
-                 lambda: file_content + Recorder._report_tail,
-                 lambda: file_content + Recorder._plan_record_tail + Recorder._report_tail]
+                 # The bigger the file_content (record file size),
+                 # the slower the appending speed (you need to copy the whole list),
+                 # but thankfully this method runs only once per program running.
+                 lambda: file_content + REPORT_TAIL]
 
         errors = []
         for case in cases:
@@ -162,14 +142,10 @@ class Recorder(object):
                 result = orjson.loads(case())
 
                 # remove additional empty items added by this class when writing report.
-                # each list's first and last item is empty item.
-                plans = result['plans'][1:-1]
-                result['plans'] = plans
-                for i in range(len(plans)):
-                    result['plans'][i]['records'] = plans[i]['records'][1:-1]
+                # "records" list's first and last item is empty item.
+                result['records'] = result['records'][1:-1]
 
                 return result
-
             except orjson.JSONDecodeError as e:
                 errors.append(e)
 
