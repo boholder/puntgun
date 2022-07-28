8641ff715850dc7762ac02fd0f8eb6ec0f6e1280
boholder <bottleholder@anche.no>
2022-07-28 16:19:32 +0800

    Satisfied with logging config

--- puntgun/conf/config.py
+++ puntgun/conf/config.py
@@ -1,5 +1,6 @@
 """All the loaded settings and global variables for many modules to use."""
 import sys
+from datetime import datetime
 from importlib.metadata import version
 
 from dynaconf import Dynaconf
@@ -12,8 +13,9 @@ from loguru import logger
 # Works sort of like java's serial version uid.
 puntgun_version = version('puntgun')
 
-# where to find the config file:
-# .../<project-root>/conf
+# TODO 这两个global配置文件应该变到用户目录下(的 .puntgun 目录)
+# Path.home()
+# sys.__stdin__.isatty()
 config_dir_path = from_root('conf')
 
 # the dumped private key is stored in the config directory
@@ -22,32 +24,62 @@ pri_key_file_path = config_dir_path.joinpath('.puntgun_rsa4096')
 # encrypted secrets are stored into this file
 secrets_config_file_path = config_dir_path.joinpath('.secrets.yml')
 
+config_files = [config_dir_path.joinpath('settings.yml'),
+                secrets_config_file_path]
+
 settings = Dynaconf(
     # environment variables' prefix
     envvar_prefix='BULLET',
-    # tool settings on the "global" level
-    settings_files=[str(config_dir_path.joinpath('settings.yml')),
-                    str(secrets_config_file_path)],
+    settings_files=config_files,
     apply_default_on_none=True
 )
 
-# settings.configure(settings_files=static_config_files + ['conf/a.yml'])
-# https://www.dynaconf.com/settings_files/#yaml-caveats
-# https://loguru.readthedocs.io/en/stable/api/logger.html#loguru._logger.Logger.configure
+
+def load_plan_configuration_file(path):
+    # Dynaconf works in a layered override mode based on the above order,
+    # the precedence will be based on the loading order.
+    # *manually tested and searched: https://dynaconf.readthedocs.io/en/docs_223/guides/usage.html
+    settings.configure(settings_files=[path] + config_files)
 
 
-# report file logger sink,
-# we're borrowing function of loguru logging library for writing an execution report file.
-# See puntgun.record.Recorder for more info.
-#
-# Add this sink only when not in unit testing,
-# or it will generate the file every time tests running, sort of annoying.
+def log_file_naming(suffix: str):
+    # loguru will follow given path to create log files.
+    # so the log files will be generated under same directory with plan configuration file.
+    # TODO 加上plan config路径
+    plan_config_file = config_dir_path.joinpath('puntgun')
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    return f"{plan_config_file}_{time}_{suffix}"
+
+
+# Configurate logging options only when not in unit testing,
+# or it will generate files every time tests running, sort of annoying.
 if "pytest" not in sys.modules:
-    logger.add('record.json',
+    # remove default logging sink (stderr)
+    logger.remove()
+
+    # source: https://github.com/Delgan/loguru/issues/586#issuecomment-1030819250
+    logger_format = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        "<level>{level: <5}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+        "<level>{message}</level>"
+    )
+
+    # console log
+    logger.add(sys.stderr,
+               filter=lambda record: 'r' not in record['extra'],
+               format=logger_format)
+
+    # log file
+    logger.add(log_file_naming('_running.log'),
+               filter=lambda record: 'r' not in record['extra'],
+               format=logger_format,
+               rotation=settings.get('log_rotation', '100 MB'))
+
+    # report file
+    # we're borrowing function of loguru library for writing execution report files.
+    # See puntgun.record.Recorder for more info.
+    logger.add(log_file_naming('_report.json'),
                filter=lambda record: 'r' in record['extra'],
                # only write the plain message content
                format='{message}')
-
-# TODO 配置文件后面加后缀
-# TODO 定义配置优先级（配置文件路径的指定）
-# https://pydantic-docs.helpmanual.io/usage/settings/#changing-priority
