a44726d6762cd11c8c7a66c50684a121b312aeb5
boholder <bottleholder@anche.no>
2022-07-29 17:50:09 +0800

    Update config.py to accept config file paths update from command line arguments.

--- puntgun/conf/config.py
+++ puntgun/conf/config.py
@@ -1,11 +1,16 @@
-"""All the loaded settings and global variables for many modules to use."""
+"""
+All the loaded settings and global variables for many modules to use.
+No unit tests guard (too implement-coupling to be valuable enough writing test cases), stay sharp.
+"""
 import sys
 from datetime import datetime
 from importlib.metadata import version
+from pathlib import Path
 
 from dynaconf import Dynaconf
-from from_root import from_root
 from loguru import logger
+
+# === configs won't changed ===
 
 # For version based branch logic in report-based "undo" operation.
 # (you have different available actions at different version,
@@ -13,47 +18,71 @@ from loguru import logger
 # Works sort of like java's serial version uid.
 puntgun_version = version('puntgun')
 
-# TODO 这两个global配置文件应该变到用户目录下(的 .puntgun 目录)
-# Path.home()
-# sys.__stdin__.isatty()
-config_dir_path = from_root('conf')
+# The tool's behavior changes base on whether it's facing an interactive shell
+# 1. when is_atty=true, the tool read password through input();
+#    when false, take stdin input as password.
+is_atty = sys.__stdin__.isatty()
 
-# the dumped private key is stored in the config directory
-pri_key_file_path = config_dir_path.joinpath('.puntgun_rsa4096')
+# === configs which final values depends on command line arguments ===
 
-# encrypted secrets are stored into this file
-secrets_config_file_path = config_dir_path.joinpath('.secrets.yml')
+config_path = Path.home().joinpath('.puntgun')
+plan_file = config_path.joinpath('plan.yml')
+settings_file = config_path.joinpath('settings.yml')
+pri_key_file = config_path.joinpath('.puntgun_rsa4096')
+secrets_file = config_path.joinpath('.secrets.yml')
 
-config_files = [config_dir_path.joinpath('settings.yml'),
-                secrets_config_file_path]
 
+def naming_log_file(suffix: str):
+    # YYYYMMDDHHMMSS
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    # loguru will follow given path to create log files.
+    # so the log files will be generated under same directory with plan configuration file.
+    return f"{plan_file}_{time}_{suffix}"
+
+
+report_file = naming_log_file('_report.json')
+
+# Dynaconf works in a layered override mode based on the above order,
+# the precedence will be based on the loading order.
+# *manually tested and searched: https://dynaconf.readthedocs.io/en/docs_223/guides/usage.html
+config_files_order = [plan_file, settings_file, secrets_file]
+
+# load configuration files
 settings = Dynaconf(
     # environment variables' prefix
     envvar_prefix='BULLET',
-    settings_files=config_files,
+    settings_files=config_files_order,
     apply_default_on_none=True
 )
 
 
-def load_plan_configuration_file(path):
-    # Dynaconf works in a layered override mode based on the above order,
-    # the precedence will be based on the loading order.
-    # *manually tested and searched: https://dynaconf.readthedocs.io/en/docs_223/guides/usage.html
-    settings.configure(settings_files=[path] + config_files)
+def reload_config_files_base_on_cmd_args(_config_path, _plan_file, _settings_file,
+                                         _private_key_file, _secrets_file, _report_file):
+    # Ugly. But don't know how to improve it.
+    global config_path
+    global plan_file
+    global settings_file
+    global pri_key_file
+    global secrets_file
+    global report_file
+
+    # after changing config_path, other paths need to be re-computed to propagate change
+    config_path = Path(_config_path) if _config_path else Path.home().joinpath('.puntgun')
+    plan_file = _plan_file if _plan_file else config_path.joinpath('plan.yml')
+    settings_file = _settings_file if _settings_file else config_path.joinpath('settings.yml')
+    pri_key_file = _private_key_file if _private_key_file else config_path.joinpath('.puntgun_rsa4096')
+    secrets_file = _secrets_file if secrets_file else config_path.joinpath('.secrets.yml')
+    report_file = _report_file if _report_file else naming_log_file('_report.json')
+
+    # reload configuration files
+    settings.configure(settings_files=config_files_order)
 
 
-def log_file_naming(suffix: str):
-    # loguru will follow given path to create log files.
-    # so the log files will be generated under same directory with plan configuration file.
-    # TODO 加上plan config路径
-    plan_config_file = config_dir_path.joinpath('puntgun')
-    time = datetime.now().strftime('%Y%m%d%H%M%S')
-    return f"{plan_config_file}_{time}_{suffix}"
+def config_logging_options():
+    """
+    Logs about current executing process to stdout will go with print(), not so much lines.
+    """
 
-
-# Configurate logging options only when not in unit testing,
-# or it will generate files every time tests running, sort of annoying.
-if "pytest" not in sys.modules:
     # remove default logging sink (stderr)
     logger.remove()
 
@@ -71,7 +100,7 @@ if "pytest" not in sys.modules:
                format=logger_format)
 
     # log file
-    logger.add(log_file_naming('_running.log'),
+    logger.add(naming_log_file('_running.log'),
                filter=lambda record: 'r' not in record['extra'],
                format=logger_format,
                rotation=settings.get('log_rotation', '100 MB'))
@@ -79,7 +108,13 @@ if "pytest" not in sys.modules:
     # report file
     # we're borrowing function of loguru library for writing execution report files.
     # See puntgun.record.Recorder for more info.
-    logger.add(log_file_naming('_report.json'),
+    logger.add(report_file,
                filter=lambda record: 'r' in record['extra'],
                # only write the plain message content
                format='{message}')
+
+
+# Configurate logging options only when not in unit testing,
+# or it will generate files every time tests running, sort of annoying.
+if "pytest" not in sys.modules:
+    config_logging_options()
