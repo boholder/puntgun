fc94c780006fcf83dfb24d551b0e1b6cd6e2e162
boholder <bottleholder@anche.no>
2022-08-06 17:40:21 +0800

    Add secret dump command line argument Bump pyhamcrest from 2.0.3 to 2.0.4 (#13)
    Bumps [pyhamcrest](https://github.com/hamcrest/PyHamcrest) from 2.0.3 to 2.0.4.
    - [Release notes](https://github.com/hamcrest/PyHamcrest/releases)
    - [Changelog](https://github.com/hamcrest/PyHamcrest/blob/main/CHANGELOG.rst)
    - [Commits](https://github.com/hamcrest/PyHamcrest/compare/V2.0.3...V2.0.4)
    
    ---
    updated-dependencies:
    - dependency-name: pyhamcrest
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ puntgun/__main__.py
@@ -0,0 +1,16 @@
+import fire
+
+from command import Command
+
+banner = r"""
+     ____              _      ____
+,___|____\____________|_|____/____|____________________
+|___|_|_)_|_|_|_|_'__\|___|_|_|___|_|_|_|_'__\__[____]  ""-,___..--=====
+    |  __/| |_| | | | | |_  | |_| | |_| | | | |   \\_____/   ""        |
+    |_|    \__,_|_| |_|\__|  \____|\__,_|_| |_|      [ ))"---------..__|
+puntgun - a configurable automation command line tool for Twitter
+"""
+
+if __name__ == '__main__':
+    print(banner)
+    fire.Fire(Command)
---
+++ puntgun/command.py
@@ -0,0 +1,55 @@
+import runner
+from conf import config
+from conf.encrypto import load_or_generate_private_key
+from conf.secret import load_or_request_all_secrets
+
+
+class Gen(object):
+    """Generate various files from the tool."""
+
+    @staticmethod
+    def secrets(secrets_file=str(config.secrets_file),
+                # this path will only be used here, so we needn't add it to config module.
+                output_file=str(config.config_path.joinpath('.secrets_plaintext.yml'))):
+        """Extract secrets from secrets file and save them in plaintext format."""
+        config.reload_config_files(secrets_file=secrets_file)
+        secrets = load_or_request_all_secrets(load_or_generate_private_key())
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.writelines(f'{key}: {value}\n' for key, value in secrets.items())
+
+    @staticmethod
+    def config(output_file):
+        """Generate example configuration files."""
+        # https://stackoverflow.com/questions/53454049/how-to-include-a-text-file-in-a-python-installed-package
+        print('config')
+
+
+class Command(object):
+
+    def __init__(self):
+        self.gen = Gen()
+
+    @staticmethod
+    def fire(config_path=str(config.config_path),
+             plan_file=str(config.plan_file),
+             settings_file=str(config.settings_file),
+             private_key_file=str(config.pri_key_file),
+             secrets_file=str(config.secrets_file),
+             report_file=str(config.report_file)):
+        """
+        Run a plan configuration.
+
+        :param config_path: Path of various configuration files the tool needs.
+        :param plan_file: Plan configuration file you'd like to execute.
+        :param settings_file: Global tool settings that will apply to every execution.
+        :param private_key_file: Tool generated password protected private key.
+        :param secrets_file: Tool generated cipher text or user writen plain text file contains secrets.
+        :param report_file: Expect path of the tool generated execution report.
+        """
+        config.reload_config_files(config_path=config_path,
+                                   plan_file=plan_file,
+                                   settings_file=settings_file,
+                                   pri_key_file=private_key_file,
+                                   secrets_file=secrets_file,
+                                   report_file=report_file)
+        runner.start()
