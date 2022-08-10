3debdd6fe05f95006f619fe4bbd392495abbe053
boholder <bottleholder@anche.no>
2022-08-10 10:18:52 +0800

    Add example config files generating sub command

--- puntgun/conf/example.py
+++ puntgun/conf/example.py
@@ -1,5 +1,12 @@
+"""
+This might be a bad idea...
+save all content of example config files as text blocks.
+Any better idea?
+"""
+
+tool_settings = """
 # The settings of the tool itself.
-# Can be overridden by the same-name ones in the user-writen plan configuration file
+# Options can be overridden by the same-name ones in the user-writen plan configuration file
 # or in the environment variables.
 # The values of the commented out options are default values
 # Reference document: TODO
@@ -12,3 +19,10 @@
 
 # Change log files writing method (when to split a new file)
 #log_rotation: 100MB
+"""
+
+plan_config = """
+# This is an example configuration file of plan.
+# Reference document: TODO
+ 
+"""
