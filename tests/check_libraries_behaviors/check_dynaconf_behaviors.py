a7eceb5a71ef986efbf4bd250a30eddae7fd4ca1
boholder <bottleholder@anche.no>
2022-08-22 14:55:47 +0800

    Remove from_root dependency which is unused now.

--- tests/check_libraries_behaviors/check_dynaconf_behaviors.py
+++ tests/check_libraries_behaviors/check_dynaconf_behaviors.py
@@ -1,10 +1,11 @@
 from dynaconf import Dynaconf
-from from_root import from_here
+
+import conf.config
 
 
 def print_loaded_settings_file():
     settings = Dynaconf(
-        settings_files=[from_here().joinpath('settings.yml')],
+        settings_files=[conf.config.config_path.joinpath('settings.yml')],
         apply_default_on_none=True
     )
     print(settings.get('s'))
